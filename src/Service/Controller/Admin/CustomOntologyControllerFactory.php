afc0f0dd04f74b1a677df3e4fd117c9de8fe2780
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Cleaned code.

--- src/Service/Controller/Admin/CustomOntologyControllerFactory.php
+++ src/Service/Controller/Admin/CustomOntologyControllerFactory.php
@@ -7,7 +7,7 @@ use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class CustomOntologyControllerFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $services, $requestedNamed, array $options = null)
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
         $entityManager = $services->get('Omeka\EntityManager');
         return new CustomOntologyController(
